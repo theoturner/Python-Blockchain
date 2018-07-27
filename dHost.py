6ea9184bf877ecc02d098b8d8bd14c4721b1c6c0
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-07-27 13:01:59 +0100

    Cleanup, def PoW function

--- dHost.py
+++ dHost.py
@@ -1,20 +1,33 @@
-# dHost - a node in the network
+"""
+dHost - a node in the network.
+Uses SHA256 as its cryptographic hash function.
+"""
 
-# We use SHA256 as our cryptographic hash function
 from hashlib import sha256
 import json
 
-# Block
-# A group of records added to the blockchain in each
-# appending operation
+"""
+Block
+A group of records added to the blockchain in each
+appending operation. For record-keeping, merkle trees are
+unnecessary.
+"""
 class Block:
+
     def __init__(self, identifier, last, tx, timestamp):
         self.identifier = identifier
         self.last = last # Hash of preceding block
         self.tx = tx # Transactions
         self.timestamp = timestamp
 
-# Blockchain
-# Essentially a singly linked list of blocks but referring to
-# the previous block with a hash, rather than a pointer
+    """
+    Calculate hash on block.
+    """
+    def proveWork(self):
+
+"""
+Blockchain
+Essentially a singly linked list of blocks but referring to
+the previous block with a hash, rather than a pointer.
+"""
 class Blockchain:
