c0e9ca2bceb7b72a3b4b6cc904cdc718404333c4
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-07-27 12:54:03 +0100

    Block constructor, fix atom auto comment replace

--- dHost.py
+++ dHost.py
@@ -1,14 +1,19 @@
-// dHost - a node in the network
+# dHost - a node in the network
 
-// We use SHA256 as our cryptographic hash fucntion
+# We use SHA256 as our cryptographic hash fucntion
 from hashlib import sha256
 
-// Block
-// A group of records added to the blockchain in each
-// appending operation
+# Block
+# A group of records added to the blockchain in each
+# appending operation
 class Block:
+    def __init__(self, identifier, last, tx, timestamp):
+        self.identifier = identifier
+        self.last = last # Hash of preceding block
+        self.tx = tx # Transactions
+        self.timestamp = timestamp
 
-// Blockchain
-// Essentially a singly linked list of blocks but referring to
-// the previous block with a hash, rather than a pointer
+# Blockchain
+# Essentially a singly linked list of blocks but referring to
+# the previous block with a hash, rather than a pointer
 class Blockchain:
