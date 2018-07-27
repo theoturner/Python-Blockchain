be6e96388b2021433dad9a6a2673056a99a080da
Theo Turner <theo@Theos-MacBook-Pro.local>
2018-07-27 12:47:52 +0100

    dHost commenting

--- dHost.py
+++ dHost.py
@@ -1,5 +1,14 @@
+// dHost - a node in the network
+
+// We use SHA256 as our cryptographic hash fucntion
 from hashlib import sha256
 
+// Block
+// A group of records added to the blockchain in each
+// appending operation
 class Block:
 
+// Blockchain
+// Essentially a singly linked list of blocks but referring to
+// the previous block with a hash, rather than a pointer
 class Blockchain:
